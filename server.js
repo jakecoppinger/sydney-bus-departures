8b236b71e120b577ca0a464b3bfa65675364d745
Jake Coppinger <jake@jakecoppinger.com>
2017-07-16 22:17:35 +1000

    Added departures endpoint

--- server.js
+++ server.js
@@ -9,34 +9,18 @@ const DeparturesFormatter = require('./DeparturesFormatter.js');
 
 const apikey = process.env.TFNSW_KEY;
 
-
 app.use(express.static('public'));
 
-
 app.get('/', function (req, res) {
-  res.send('Hello World!' + new Date());
+  res.send('Hello World!');
 });
-
-
-/*
-Route path: /users/:userId/books/:bookId
-Request URL: http://localhost:3000/users/34/books/8989
-req.params: { "userId": "34", "bookId": "8989" }
-To define routes with route parameters, simply specify the route parameters in the path of the route as shown below.
-
-app.get('/users/:userId/books/:bookId', function (req, res) {
-  res.send(req.params)
-})
-*/
-
-//http://server/url?array=["foo","bar"]
 
 app.get('/v1/summary', function (req, res) {
     let stopID = req.query.stop;
     let routes = req.query.routes.split(',');
     let num = req.query.num;
 
-    console.log("query: ", req.query);
+    console.log(`/v1/summary  stopID: $(stopID), routes:${JSON.stringify(routes)}, num:${num}`);
 
     let stop = new BusStopDepartures(apikey);
 
@@ -46,17 +30,14 @@ app.get('/v1/summary', function (req, res) {
     });
 });
 
-app.get('/test', function (req, res) {
-    res.send("hello " + new Date()); //formatter.routeSummaryString(["396", "394"], 3));
-    res.end();
-});
+app.get('/v1/departures', function (req, res) {
+    let stopID = req.query.stop;
+    console.log(`/v1/departures  stopID: ${stopID}`);
 
-
-app.post('/test', function(req, res) {
-    console.log("hell wo");
-    console.log(req.headers);
-
-    res.send("hello");
+    let stop = new BusStopDepartures(apikey);
+    stop.getDeparturesForStop(stopID, () => {
+        res.send(stop.data());
+    });
 });
 
 var port = process.env.PORT || 3000;
