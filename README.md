62e6fc6e5214ff625f4fed513e97596c1795e942
Jake Coppinger <jake@jakecoppinger.com>
2017-07-16 19:24:47 +1000

    Added proper files to dependencies

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ https://www.npmjs.com/package/uber-json-schema-filter
 
 ## Also handy
 https://nbviewer.jupyter.org/gist/timbennett/7ec739fc619459316859d3875b76a76b/notebook.ipynb
+
+## Example request
+
+http://localhost:3000/v1/summary?stop=203220&routes=396,394&num=3
--- server.js
+++ server.js
@@ -46,23 +46,6 @@ app.get('/v1/summary', function (req, res) {
     });
 });
 
-app.get('/v1/closestfavourites', function (req, res) {
-    let stopID = req.query.stop;
-    let routes = req.query.routes.split(',');
-    let num = req.query.num;
-
-    console.log("query: ", req.query);
-
-    let stop = new BusStopDepartures(apikey);
-
-    stop.getDeparturesForStop(stopID, () => {
-        const formatter = new DeparturesFormatter(stop.data());
-        res.send(formatter.routeSummaryString(routes, 3));
-    });
-});
-
-
-
 app.get('/test', function (req, res) {
     res.send("hello " + new Date()); //formatter.routeSummaryString(["396", "394"], 3));
     res.end();
